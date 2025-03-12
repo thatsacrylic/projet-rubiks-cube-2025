328eb758423d372c5aab2d846190134569c8e3fc
Acrylic <49649756+thatsacrylic@users.noreply.github.com>
2025-03-12 10:29:21 +0100

    Prototype de fichier visoCube.py

--- mainCube.py
+++ mainCube.py
@@ -9,9 +9,6 @@ import ihmCube
 import modeleCube
 import robotCube
 
-
-
-
 class Main:
     """
     Application Robot Rubik's cube.
---
+++ visoCube.py
@@ -0,0 +1,46 @@
+"""
+Yann LANCELIN / Esteban-Angel GONZALEZ-DURAND
+03/2025
+
+V1:
+Reconnaissance du cube à partir de la caméra
+"""
+
+class Reconnaissance:
+    def __init__(self):
+        pass
+    
+    def parametrer(self):
+        pass
+    
+    def sauvegardeJSON(self):
+        pass 
+    
+    def recuperationJSON(self):
+        pass
+    
+class IHMvisio:
+    def __init__(self):
+        pass
+    
+    def waitClick(self):
+        return ""
+    
+    def refresh(self):
+        pass
+    
+    def appendCamera(self, cam):
+        pass
+    
+class Camera:
+    def __init__(self):
+        pass
+    
+    def prendrePhoto(self):
+        pass
+
+class pointMesure:
+    def __init__(self):
+        pass
+    
+    pass
